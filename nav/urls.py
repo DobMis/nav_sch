edc6634322a9ae92b6d02f9afac47663e2e7a3b1
DobMis <dobrydinmikhail@gmail.com>
2025-02-18 17:49:06 +0300

    sth

--- nav/urls.py
+++ nav/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),  # Проверяем этот маршрут
+    path('about/', views.about, name='about'),
+    path('map/', views.map_view, name='map'),
+    path('rooms/', views.rooms, name='rooms'),
+    path('room/<int:room_number>/', views.room_view, name='room'), 
 ]
-
--- nav/views.py
+++ nav/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
     return render(request, 'home.html')
+
 def about(request):
     return render(request, 'about.html')
+
+def map_view(request):
+    return render(request, 'map.html')
+
+def rooms(request):
+    return render(request, 'rooms.html')
+
+def room_view(request, room_number):
+    return render(request, 'room.html', {'room_number': room_number})
